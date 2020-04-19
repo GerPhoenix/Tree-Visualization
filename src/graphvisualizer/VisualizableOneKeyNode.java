1d4c08fe23535bfc249f1a568640557685afbb49
Jonas <jonashoefler@gmx.de>
2020-04-19 18:33:34 +0200

    [VisualizableOneKeyNode] fixed typo in documentation

--- src/graphvisualizer/VisualizableOneKeyNode.java
+++ src/graphvisualizer/VisualizableOneKeyNode.java
@@ -4,7 +4,7 @@ package graphvisualizer;
  * Implements the VisualizableNode Interface which has to be implemented by tree nodes if you want to use the {@link TreeVisualizer}.
  * <br>
  * <br>
- * This interface helps writing nodes with only one key by adding a default implementation of {@link VisualizableOneKeyNode#getKeys() getKeys} relying on a add {@link VisualizableOneKeyNode#getKey() getKey} function
+ * This interface helps writing nodes with only one key by adding a default implementation of {@link VisualizableOneKeyNode#getKeys() getKeys} relying on the {@link VisualizableOneKeyNode#getKey() getKey} function
  * see {@link VisualizableNode}
  */
 public interface VisualizableOneKeyNode extends VisualizableNode {
